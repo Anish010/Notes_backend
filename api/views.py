d3c0aaff5b6ca7e82586a0c67e19c3b9699c7ee1
Anish010 <anishmohanty101@gmail.com>
2023-08-02 22:26:24 +0530

    Notes app backend

---
+++ api/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
+# Create your views here.
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting note'
+        },
+    ]
+    return Response(routes) #Safe=False means can reture more data than python dictionary;
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many = True) #many = True bcoz we need to serialize more than 1 object
+    #serializer itself is an object type
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    
+    #If id is passed using params Ex : /note?id=2
+    # param = request.GET.get('id')
+    notes = Note.objects.get(id=pk)
+    
+    serializer = NoteSerializer(notes, many = False)  #many = False bcoz we need to serialize only 1 object
+    #serializer itself is an object type
+    return Response(serializer.data) 
+
+@api_view(['POST'])
+def createNote(request):
+    data = request.data
+    note = Note.objects.create(
+        body=data['body']
+    )
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(instance=note, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response('Note was deleted!')
